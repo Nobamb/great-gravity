95dc7730478f17119600683910de00f52b5be972
nobamb <jsh12345644@gmail.com>
2026-04-05 20:38:53 +0900

    feat: 3stage 추가

--- react-stage/src/stages/stageDefinitions.js
+++ react-stage/src/stages/stageDefinitions.js
@@ -9,9 +9,16 @@ export const STAGE_DEFINITIONS = {
     stage2: {
         id: "stage2",
         path: "/stage2",
+        nextStageId: "stage3",
+        supportsNextStage: true,
+        title: "Stage 2",
+    },
+    stage3: {
+        id: "stage3",
+        path: "/stage3",
         nextStageId: null,
         supportsNextStage: false,
-        title: "Stage 2",
+        title: "Stage 3",
     },
 };
 
