b11a1691caa52a77d5a471b80e1ee65ae6b7786a
nobamb <jsh12345644@gmail.com>
2026-04-06 23:18:56 +0900

    feat: 커스텀 미션 알람 컴포넌트 추가

---
+++ react-stage/src/components/CustomMissionAlarm.jsx
@@ -0,0 +1,12 @@
+export default function CustomMissionAlarm() {
+    return (
+        <div className="custom-mission-alarm" hidden>
+            <div className="custom-mission-alarm__card">
+                <div
+                    className="custom-mission-alarm__text"
+                    data-custom-mission-alarm-text="true"
+                ></div>
+            </div>
+        </div>
+    );
+}
