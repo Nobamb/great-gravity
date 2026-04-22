64249249bc4fc280904852c9c4c0b036b23bb124
nobamb <jsh12345644@gmail.com>
2026-04-22 15:31:58 +0900

    feat: 다국어 시스템 초기화를 위해 i18n/index.js 연동

--- react-stage/src/main.jsx
+++ react-stage/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./i18n/index.js";
 import "./stage.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
