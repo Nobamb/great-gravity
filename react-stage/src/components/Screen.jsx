710a4d36b6ae31cbcec47331c7958c4e2b19d6d5
nobamb <jsh12345644@gmail.com>
2026-04-23 15:36:23 +0900

    feat: 모바일 가로/세로 모드 대응을 위한 Screen 컴포넌트 클래스 동적 바인딩 추가

--- react-stage/src/components/Screen.jsx
+++ react-stage/src/components/Screen.jsx
@@ -1,8 +1,16 @@
 import PreferencesModal from "./PreferencesModal.jsx";
+import { usePreferences } from "../contexts/PreferencesContext.jsx";
 
 export default function Screen({ children }) {
+    const { isMobileViewport, isLandscapeScreen } = usePreferences();
+    const screenClasses = [
+        isMobileViewport ? "screen--mobile" : "",
+        isMobileViewport && isLandscapeScreen ? "screen--mobile-landscape" : "",
+        isMobileViewport && !isLandscapeScreen ? "screen--mobile-portrait" : "",
+    ].filter(Boolean).join(" ");
+
     return (
-        <div id="screen">
+        <div id="screen" className={screenClasses}>
             {children}
             <PreferencesModal />
         </div>
