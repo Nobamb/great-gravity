dc80c459b5a68f06001796d75fc09daba5ed2c7f
nobamb <jsh12345644@gmail.com>
2026-04-23 17:04:53 +0900

    feat: Screen 컴포넌트에 기기 방향(Viewport Landscape/Portrait) 클래스 추가

--- react-stage/src/components/Screen.jsx
+++ react-stage/src/components/Screen.jsx
@@ -2,9 +2,11 @@ import PreferencesModal from "./PreferencesModal.jsx";
 import { usePreferences } from "../contexts/PreferencesContext.jsx";
 
 export default function Screen({ children }) {
-    const { isMobileViewport, isLandscapeScreen } = usePreferences();
+    const { isMobileViewport, isViewportLandscape, isLandscapeScreen } = usePreferences();
     const screenClasses = [
         isMobileViewport ? "screen--mobile" : "",
+        isMobileViewport && isViewportLandscape ? "screen--viewport-landscape" : "",
+        isMobileViewport && !isViewportLandscape ? "screen--viewport-portrait" : "",
         isMobileViewport && isLandscapeScreen ? "screen--mobile-landscape" : "",
         isMobileViewport && !isLandscapeScreen ? "screen--mobile-portrait" : "",
     ].filter(Boolean).join(" ");
