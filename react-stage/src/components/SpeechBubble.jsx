2f280fd2732ea6446c6d50c5cd5f7bd41df6e843
nobamb <jsh12345644@gmail.com>
2026-04-27 00:05:17 +0900

    feat: 어떤 상황에서도 말풍선의 내용 유동적으로 넣을 수 있도록 지정

--- react-stage/src/components/SpeechBubble.jsx
+++ react-stage/src/components/SpeechBubble.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-function SpeechBubble({ isVisible }) {
+function SpeechBubble({
+    isVisible,
+    desktopKey = "common.settingsTooltipFull",
+    mobileKey = "common.settingsTooltipShort",
+}) {
     const { t } = useTranslation();
 
     return (
         <div className={`speech-bubble ${isVisible ? "speech-bubble--visible" : ""}`}>
-            <span className="speech-bubble__text--desktop">{t("common.settingsTooltipFull")}</span>
-            <span className="speech-bubble__text--mobile">{t("common.settingsTooltipShort")}</span>
+            <span className="speech-bubble__text--desktop">{t(desktopKey)}</span>
+            <span className="speech-bubble__text--mobile">{t(mobileKey)}</span>
         </div>
     );
 }
